430f67ddca4bdf56a2ee83e7d64d9c82ad7ac0fe
Pranav <pranavtaukari@gmail.com>
2022-10-05 00:12:44 -0500

    Heroku Setup- First Time

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn MIRC-Gradebook.wsgi:application --log-file -
--- gradebook_project/settings.py
+++ gradebook_project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from email.charset import BASE64
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-zlr-5q$fn*wrd$wt1o8*4ecrx$!x*0!n@i07grfm^)u3rmceds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -146,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
