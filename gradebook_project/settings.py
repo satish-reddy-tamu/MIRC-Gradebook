4f64f7fe73db75ef122440e01fe0cec3c58b6515
satishreddy <satish_reddy@tamu.edu>
2022-10-06 22:22:08 -0500

    templates directory moved

--- gradebook_project/settings.py
+++ gradebook_project/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'gradebook_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'gradebook_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
