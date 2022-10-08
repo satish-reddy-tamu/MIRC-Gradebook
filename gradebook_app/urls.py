4d0c0bece054574176a3951eaa78dfdd758647e1
satishreddy <satish_reddy@tamu.edu>
2022-10-07 23:17:40 -0500

    different user types created

--- gradebook_app/urls.py
+++ gradebook_app/urls.py
@@ -1,12 +1,10 @@
-from django.shortcuts import redirect
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', lambda request: redirect('home/', permanent=False)),
-    path('accounts/', include('allauth.urls')),
-    path('home/', views.home),
-    path('login/', views.login),
+    path('', views.home, name='home'),
+    path('login', views.login),
+    path('accounts/', include('allauth.urls'))
 ]
