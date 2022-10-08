b78578e84ae90db8cf804cc78611e860b7576494
satishreddy <satish_reddy@tamu.edu>
2022-10-07 22:34:29 -0500

    different user types created

--- gradebook_app/urls.py
+++ gradebook_app/urls.py
@@ -4,8 +4,8 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/home')),
-    path('/home', views.home),
-    path('/login', views.login),
-    path('/accounts/', include('allauth.urls')),
+    path('', RedirectView.as_view(url='home')),
+    path('home', views.home),
+    path('login', views.login),
+    path('accounts/', include('allauth.urls')),
 ]
