365b053ecd0b75d3c81f919d918efc61fbcb1cb7
satishreddy <satish_reddy@tamu.edu>
2022-10-07 23:19:49 -0500

    different user types created

--- gradebook_app/urls.py
+++ gradebook_app/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
     path('login', views.login),
     path('accounts/', include('allauth.urls'))
 ]
