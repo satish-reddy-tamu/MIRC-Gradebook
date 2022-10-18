dab9c10054105e49ff856cdac5108b3e16cb8af4
satishreddy <satish_reddy@tamu.edu>
2022-10-18 11:41:02 -0500

    new course model added and bulk profiles addition feature added

--- gradebook_app/urls.py
+++ gradebook_app/urls.py
@@ -2,11 +2,17 @@ from django.urls import path, include
 from django.views.generic import TemplateView, RedirectView
 
 from .views.common import login_view
+from gradebook_app.views.admin import profile_view
 
 urlpatterns = [
     path('', RedirectView.as_view(url='home')),
     path('home', login_view.home),
     path('login', login_view.login),
     path('contact', TemplateView.as_view(template_name="common/contact.html")),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+
+    # admin
+    path('admin/profiles', TemplateView.as_view(template_name="admin/profiles.html")),
+    path('admin/profiles/add_bulk', profile_view.add_bulk_profiles, name='add_bulk_profiles')
+
 ]
