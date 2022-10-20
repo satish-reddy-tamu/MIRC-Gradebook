bd846ac294179618a18dc857a127c8a373747a99
satishreddy <satish_reddy@tamu.edu>
2022-10-19 21:24:23 -0500

    profiles crud operations added

--- gradebook_app/views/admin/profile_view.py
+++ gradebook_app/views/admin/profile_view.py
@@ -9,39 +9,37 @@ from gradebook_app.models.profile_model import Profile, ProfileForm
 
 def display_all_profiles(request):
     profiles = Profile.objects.all()
-    return render(request, 'admin/profiles.html', {'profiles': profiles})
+    return render(request, 'admin/profiles.html', {
+        'profiles': profiles,
+        'add_profile_form': ProfileForm()
+    })
 
 
 def add_profile(request):
-    if request.method == "POST":
-        form = ProfileForm(request.POST)
-        if form.is_valid():
-            try:
-                form.save()
-                return redirect(display_all_profiles)
-            except:
-                pass
+    form = ProfileForm(request.POST)
+    if form.is_valid():
+        try:
+            form.save()
+            return redirect(display_all_profiles)
+        except:
+            print("save failed")
     else:
-        form = ProfileForm()
-    return render(request, 'admin/add_profile.html', {'profile_form': form})
+        print("invalid form")
 
 
 def add_bulk_profiles(request):
     try:
-        if request.method == "POST":
-            profiles_csv_file = TextIOWrapper(request.FILES['profiles_csv_file'], encoding=request.encoding)
-            profiles_list = []
-            for row in DictReader(profiles_csv_file):
-                profile = Profile(
-                    email=row['email'],
-                    name=row['name'],
-                    type=row['type']
-                )
-                profiles_list.append(profile)
-            Profile.objects.bulk_create(profiles_list)
-            return redirect(display_all_profiles)
-        else:
-            return render(request, 'admin/add_bulk_profiles.html')
+        profiles_csv_file = TextIOWrapper(request.FILES['profiles_csv_file'], encoding=request.encoding)
+        profiles_list = []
+        for row in DictReader(profiles_csv_file):
+            profile = Profile(
+                email=row['email'],
+                name=row['name'],
+                type=row['type']
+            )
+            profiles_list.append(profile)
+        Profile.objects.bulk_create(profiles_list)
+        return redirect(display_all_profiles)
     except Exception as e:
         print(e)
         return HttpResponse("failed" + str(e))
