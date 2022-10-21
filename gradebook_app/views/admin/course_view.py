c27d0ac06efc5848692cfaae5919546e4dcf28fc
Sagar Adhikari <sagaradhikari@main-10-231-206-199.tamulink.tamu.edu>
2022-10-21 16:28:41 -0500

    added courses model

--- gradebook_app/views/admin/course_view.py
+++ gradebook_app/views/admin/course_view.py
@@ -1,9 +1,29 @@
-def add_bulk_courses():
-    pass
+from csv import DictReader
+from io import TextIOWrapper
+
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from gradebook_app.models.course_model import Course, CourseForm
+
+def display_all_courses(request):
+    courses = Course.objects.all()
+    return render(request, 'admin/courses.html', {
+        'courses': courses,
+        'add_course_form': CourseForm()
+    })
 
 
-def add_course():
-    pass
+def add_course(request):
+    form = CourseForm(request.POST)
+    if form.is_valid():
+        try:
+            form.save()
+            return redirect(display_all_courses)
+        except:
+            print("save failed")
+    else:
+        print("invalid form")
 
 
 def update_course():
