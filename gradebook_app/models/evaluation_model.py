23426ad8931cf815d9e4f702e0d96ac0d1537249
satishreddy <satish_reddy@tamu.edu>
2022-11-13 19:03:01 -0600

    new database models created and updated

--- gradebook_app/models/evaluation_model.py
+++ gradebook_app/models/evaluation_model.py
@@ -1,23 +1,21 @@
 import datetime
 
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from gradebook_app.models.course_model import Course
 
 
 class Evaluation(models.Model):
-    eval_id = models.CharField(max_length=10, unique=True)
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
     eval_type = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    max_score = models.IntegerField(blank=False)
-    created_at = models.DateField(default=datetime.date.today)
-    due_by = models.DateField(default=datetime.date.today)
+    weight = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
+    max_marks = models.IntegerField()
 
 
-class EvalulationForm(forms.ModelForm):
+class EvaluationForm(forms.ModelForm):
     class Meta:
         model = Evaluation
         fields = '__all__'
