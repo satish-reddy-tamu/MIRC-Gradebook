d53cff2292ae0d143a885ea57f0d8d35cfea3dcd
satishreddy <satish_reddy@tamu.edu>
2022-11-14 21:13:48 -0600

    database model foreign key correction

--- gradebook_app/models/marks_model.py
+++ gradebook_app/models/marks_model.py
@@ -6,5 +6,5 @@ from gradebook_app.models import Profile, Course, Evaluation
 class Marks(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    eval_id = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+    evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
     marks = models.FloatField(default=0)
