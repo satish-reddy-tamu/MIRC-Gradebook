f2fa6c9cc503ef67875b337214863d4257f3e41f
satishreddy <satish_reddy@tamu.edu>
2022-12-06 09:31:50 -0600

    student dashboard changes

--- gradebook_app/util/marks_util.py
+++ gradebook_app/util/marks_util.py
@@ -1,3 +1,6 @@
+from django.db.models.functions import Round
+
+
 def calculate_normalized_score(marks, max_marks, weights, total_weight, rnd=True):
     norm_score = 0
     if isinstance(marks, list):
@@ -7,7 +10,8 @@ def calculate_normalized_score(marks, max_marks, weights, total_weight, rnd=True
         norm_score = (marks / max_marks) * (weights / total_weight)
     if rnd:
         return round(norm_score * 100, 2)
-    return norm_score * 100
+    else:
+        return Round(norm_score * 100, 2)
 
 
 def calculate_grade(score, thresholds):
